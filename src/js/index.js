29f5ec0f527dab3c9e7679c598910a8dfef5fe82
Satoshi Kobayashi <koba@yk.rim.or.jp>
2023-03-07 21:33:38 +0800

    盤面操作のUIを仮登録

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,29 @@
  *  Released under the MIT license
  *  https://github.com/kobalab/UpperHand/blob/master/LICENSE
  */
+
+const $ = require('jquery');
+const Game  = require('./game');
+const Board = require('./board');
+
+$(function(){
+
+    function set_handlar(board) {
+        board._status.forEach(s=> s.off('click'));
+        if (! board._game.next) return;
+        for (let p = 0; p < board._status.length; p++) {
+            if (board._game.status(p) == -1) {
+                board._status[p].on('click', ()=>{
+                    board._game.makeMove(p);
+                    board.redraw();
+                    set_handlar(board);
+                });
+            }
+        }
+    }
+
+    const game  = new Game();
+    const board = new Board($('#board'), game, 24);
+    board.redraw();
+    set_handlar(board);
+});
