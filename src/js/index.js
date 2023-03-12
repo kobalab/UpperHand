7132530e51fe1f4af45c5ca35ecb2108d15f96b2
Satoshi Kobayashi <koba@yk.rim.or.jp>
2023-03-12 21:43:08 +0800

    URLのフラグメントで起動条件を指定できるようにした

--- src/js/index.js
+++ src/js/index.js
@@ -13,20 +13,30 @@ const Controller = require('./controller');
 
 $(function(){
 
-    let size = + location.hash.replace(/^#/,'') || 5;
+    let [ size, level, turn ] = location.hash.replace(/^#/,'').split('/');
+
+    size = + size || 5;
     size = size < 3  ?  3
          : size > 11 ? 11
          :             size;
+
+    turn = + turn || 1;
+
+    if (level == '0') turn = 0;
+
+    level = (level || 2) - 1;
+
     let r = Math.min((($('body').width() - 40) / size / 2)|0, 24);
 
-    level = 3;
-
-    function start(first) {
+    function start() {
         const game = new Game(size);
-        const ctrl = new Controller($('#board'), game, r, ()=>start(! first));
-        if (first) ctrl.start(null, new Player(game, level));
-        else       ctrl.start(new Player(game, level), null);
+        const ctrl = new Controller($('#board'), game, r, start);
+        if      (turn == 1) ctrl.start(null, new Player(game, level));
+        else if (turn == 2) ctrl.start(new Player(game, level), null);
+        else if (turn == 3) ctrl.start(new Player(game, level),
+                                       new Player(game, level));
+        else                ctrl.start(null, null);
     }
 
-    start(1);
+    start();
 });
